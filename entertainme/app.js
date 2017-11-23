05a947e31b2096a77cb0a58fdb80fcd868df80a6
lokiloko <ianw.tkj2@gmail.com>
2017-11-23 15:20:56 +0700

    add redis without auto validated

--- entertainme/app.js
+++ entertainme/app.js
@@ -1,16 +1,40 @@
 const fastify = require('fastify')()
+const responseTime = require('response-time')
+const redis = require('redis')
+var client = redis.createClient()
+
 const Movies = require('./lib/movieAPI/movies')
 const Tv = require('./lib/tvAPI/tv')
 
-fastify.get('/api/entertainme', async function (request, reply) {
-  const movieData = await Movies.getMovies()
-  const tvData = await Tv.getTvSeries()
+fastify.use(responseTime())
 
-  reply.send({
-    status: 'ok',
-    movies: movieData,
-    series: tvData
-  })
+fastify.get('/api/entertainme', function (request, reply) {
+  client.get("movies&tv", async function(err, res) {
+    if(err) {
+      console.error('redis error', err)
+      reply.send({
+        status: 'failed',
+      })
+    } else {
+      if (res) {
+        var data = JSON.parse(res)
+        reply.send({
+          status: 'ok',
+          movies: data.movies,
+          series: data.series
+        })
+      } else {
+        const movieData = await Movies.getMovies()
+        const tvData = await Tv.getTvSeries()
+        client.set('movies&tv', JSON.stringify({movies: movieData, series: tvData}));
+        reply.send({
+          status: 'ok',
+          movies: movieData,
+          series: tvData
+        })
+      }
+    }
+  });
 })
 
 fastify.listen(3000, function (err) {
