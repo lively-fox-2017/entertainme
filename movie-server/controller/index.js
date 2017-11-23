f5b61076a947ad56372332957c2eed58973203ee
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-23 13:48:41 +0700

    create movie-server

---
+++ movie-server/controller/index.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const model = require('./../model');
+const generateResponse = require('./../helpers/generate-response');
+
+const create = async (req, res) => {
+  try {
+    const created = await model.create(req.body);
+    const response = generateResponse(200, 'create new movie entry', created, null);
+  } catch (err) {
+    const response = generateResponse(500, 'failed to create new movie entry', null, err);
+  } finally {
+    res.status(response.status).send(response);
+  }
+};
+
+const findAll = async (req, res) => {
+  let response;
+
+  try {
+    const movies = await model.find().exec();
+    response = generateResponse(200, 'read all movies', movies, null);
+  } catch (err) {
+    response = generateResponse(500, 'failed to read all movies', null, err);
+  } finally {
+    res.status(response.status).send(response);
+  }
+};
+
+const  findOne = async (req, res) => {
+  let response;
+
+  try {
+    const movie = await model.findById(req.params.id);
+    response = generateResponse(200, 'read single movie', movie, null);
+  } catch (err) {
+    response = generateResponse(500, 'failed read single movie', null, err);
+  } finally {
+    res.status(response.status).send(response);
+  }
+};
+
+const update = async (req, res) => {
+  let response;
+
+  try {
+    const updated = await model.update({_id: req.params.id}, req.body);
+    if (updated.n === 0) throw new Error('movie not found');
+    response = generateResponse(200, 'update movie', updated, null);
+  } catch (err) {
+    response = generateResponse(500, 'failed to update movie', null, err);
+  } finally {
+    res.status(response.status).send(response);
+  }
+};
+
+const destroy = async (req, res) => {
+  let response;
+
+  try {
+    const destroyed = await model.destroy({_id: req.params.id});
+    if (destroyed.result.n === 0) throw new Error('movie not found');
+    response = generateResponse(200, 'delete movie', destroyed, null);
+  } catch (err) {
+    response = generateResponse(500, 'failed to delete movie', null, err);
+  } finally {
+    res.status(response.status).send(response);
+  }
+};
+
+module.exports = {
+  create,
+  findAll,
+  findOne,
+  update,
+  destroy,
+};
