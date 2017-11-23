b4fc14fb46db12208600cbe99a117344b04b7554
dimasgardenia <dimas.gardenia@gmail.com>
2017-11-23 18:39:32 +0700

    release 2 and 3 done with refactore code

--- entertaiment/controllers/entertaimentControl.js
+++ entertaiment/controllers/entertaimentControl.js
@@ -1,22 +1,62 @@
 const axios = require('axios')
+const redis = require('redis')
+const client = redis.createClient(6379)
 
-const getMovie = () => axios.get('http://localhost:3001/movies')
-const getTvSeries = () => axios.get('http://localhost:3002/series')
 
 const getApiFromMovieAndTv = async (req, res) => {
-  try {
-    const movie = await getMovie()
-    const tvSeries = await getTvSeries()
-
-    res.send({
-      movie: movie.data,
-      tvSeries: tvSeries.data
+  let moviesData = {}
+  let seriesData = {}
+  client.get(1, async (err, movies) => {
+    if (err) console.log(err)
+    client.get(2, async (err, series) => {
+      if (err) console.log(err)
+      if (movies) {
+        moviesData = movies
+      } else {
+        console.log('masuk');
+          let movies = await axios.get('http://localhost:3001/movies')
+          let fetch = movies.data
+          client.set(1, fetch)
+          moviesData = fetch.data
+      }
+      if(series) {
+        seriesData = JSON.parse(series)
+      } else {
+          let series = await axios.get('http://localhost:3002/series')
+          let fetchSeries = series.data
+          client.set(2, fetchSeries)
+          seriesData = fetchSeries.data
+      }
+      res.send({
+        movies: movies,
+        series: series
+      })
     })
-  } catch (err) {
-    console.error(err)
-  }
+  })
+}
+
+const getNewMovieFromApi = async (req, res) => {
+  let movie = await axios.get('http://localhost:3001/movies')
+  let movieData = JSON.stringify(movie.data)
+  client.set(1, movieData)
+  res.send({
+    info: 'movies found successfully',
+    data: movieData
+  })
+}
+
+const getNewSeriesFromApi = async (req, res) => {
+  let series = await axios.get('http://localhost:3002/series')
+  let seriesData = JSON.stringify(series.data)
+  client.set(2, seriesData)
+  res.send({
+    info: 'Tv series found successfully',
+    data: seriesData
+  })
 }
 
 module.exports = {
-  getApiFromMovieAndTv
+  getApiFromMovieAndTv,
+  getNewMovieFromApi,
+  getNewSeriesFromApi
 }
--- entertaiment/routers/entertaimentRoute.js
+++ entertaiment/routers/entertaimentRoute.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const tvSeries = require('../controllers/entertaimentControl')
 
 router.get('/entertaiment', tvSeries.getApiFromMovieAndTv)
+router.get('/newmovie', tvSeries.getNewMovieFromApi)
+router.get('/newseries', tvSeries.getNewSeriesFromApi)
 
 module.exports = router
