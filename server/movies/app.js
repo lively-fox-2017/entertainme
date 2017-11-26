34e04ea5d3ee6c8e2bb825480be21cf8ab690e6b
Pragistyo <new.SOS.hope@outlook.com>
2017-11-26 13:09:04 +0700

    microservice with fastifu done

--- server/movies/app.js
+++ server/movies/app.js
@@ -1,9 +1,31 @@
-const fastify = require('fastify')()
-const bodyParser = require('body-parser')
+const fastify    = require('fastify')()
+const mongoose   = require('mongoose')
+const cors       = require('cors')
+// const bodyParser = require('body-parser')
 
-fastify.use(bodyParser.json())
-fastify.use(bodyParser.urlencoded({ extended: false }))
+//mongo
+const uri = 'mongodb://localhost/entertaint-movie'
+mongoose.connect(uri, (err) => {
+    if (!err) { console.log('MOVIES CONNECTED MONGOOSE') }
+    else { console.log('MOVIES NOT CONNECTED MONGOOSE') }
+})
+//use cors
+fastify.use(cors())
 
+// fastify parsing
+fastify.register(require('fastify-formbody'), {}, (err) => {
+    if (err) throw err
+    console.log('fastify-formbody running !')
+})
+
+//route
+const movies = require('./routes/movies');
+
+//use route
+fastify.register(movies, { prefix:'/movies' })
+
+// fastify.use(bodyParser.json())
+// fastify.use(bodyParser.urlencoded({ extended: false }))
 
 
 fastify.listen(3001, () => {
--- server/tv/app.js
+++ server/tv/app.js
@@ -1,11 +1,31 @@
-const fastify = require('fastify')()
-const bodyParser = require('body-parser')
+const fastify    = require('fastify')()
+const mongoose   = require('mongoose')
+const cors       = require('cors')
 
-fastify.use(bodyParser.json())
-fastify.use(bodyParser.urlencoded({ extended: false }))
+//mongo
+const uri = 'mongodb://localhost/entertaint-tv'
+mongoose.connect(uri, (err)=>{
+    if(!err) {console.log('TV CONNECTED MONGOOSE')}
+    else { console.log('TV NOT CONNECTED MONGOOSE')}
+})
 
+// fastify parsing
+fastify.register(require('fastify-formbody'), {}, (err) => {
+    if (err) throw err
+    console.log('fastify-formbody running !')
+})
 
+//route
+const tv = require('./routes/tv');
 
-fastify.listen(3002, () =>{
+//use cors
+fastify.use(cors())
+
+// use route
+// fastify.use('/tv', tv)
+fastify.register(tv, { prefix: '/tv' })
+
+fastify.listen(3002, (err) =>{
+    if (err) throw err
     console.log('Port 3002 for TV !')
 })
