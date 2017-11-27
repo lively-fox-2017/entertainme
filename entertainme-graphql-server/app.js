8691f00cf1d2c77234b4a47b3e536b3fa94cfb6d
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-27 12:47:24 +0700

    create more queries

--- entertainme-graphql-server/app.js
+++ entertainme-graphql-server/app.js
@@ -36,7 +36,11 @@ const schema = buildSchema(`
 
   type Query {
     movies: [Movie],
-    tvs: [Tv]
+    tvs: [Tv],
+    getMovieById(_id: ID!): Movie,
+    getTvById(_id: ID!): Tv,
+    getMovieWithPopularityMoreThan(popularity: Float): [Movie],
+    getTvWithPopularityMoreThan(popularity: Float): [Tv]
   }
 `);
 
@@ -58,6 +62,56 @@ const root = {
       axios.get('http://localhost:3002/tv')
       .then(resp => {
         resolve(resp.data.payload);
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  },
+
+  getMovieById: ({_id}) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`http://localhost:3001/movie/${_id}`)
+      .then(resp => {
+        resolve(resp.data.payload);
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  },
+
+  getTvById: ({_id}) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`http://localhost:3002/tv/${_id}`)
+      .then(resp => {
+        resolve(resp.data.payload);
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  },
+
+  getMovieWithPopularityMoreThan: ({popularity}) => {
+    return new Promise((resolve, reject) => {
+      axios.get('http://localhost:3001/movie')
+      .then(resp => {
+        const movies = resp.data.payload.filter(movie => movie.popularity >= popularity)
+        resolve(movies);
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  },
+
+  getTvWithPopularityMoreThan: ({popularity}) => {
+    return new Promise((resolve, reject) => {
+      axios.get('http://localhost:3002/tv')
+      .then(resp => {
+        const tvs = resp.data.payload.filter(tv => tv.popularity >= popularity)
+        resolve(tvs);
       })
       .catch(err => {
         reject(err);
